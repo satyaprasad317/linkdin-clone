771ca9c93a4f97f877a17a6f7997471a5eea1328
“satyaprasad317” <satyaprasad317@gmail.com>
2021-07-02 20:46:41 +0530

    aaa

--- src/Components/header/HeaderOption.js
+++ src/Components/header/HeaderOption.js
@@ -5,7 +5,7 @@ function HeaderOption({ Icon, title }) {
   return (
     <div className="HeaderOption">
       {Icon && <Icon className="headerOption_Icon" />}
-      {<h3 className="HeaderOption_title">{title}</h3>}
+      {<h3 className="HeaderOption_titles">{title}</h3>}
     </div>
   );
 }
